018b21f310b8aa91389e868155488b60a8a25bac
Azreef <zhoreefm@gmail.com>
2025-04-14 10:50:30 +0800

    Initial commit

---
+++ Source/PR_Assesment/PR_Assesment.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PR_Assesment.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PR_Assesment, "PR_Assesment" );

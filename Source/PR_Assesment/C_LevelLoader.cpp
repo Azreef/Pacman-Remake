872543225fa8bad3ddc64abf67dfabea96f96f29
Azreef <zhoreefm@gmail.com>
2025-04-14 15:25:29 +0800

    Added Basic Maze Generation

---
+++ Source/PR_Assesment/C_LevelLoader.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_LevelLoader.h"
+
+// Sets default values
+AC_LevelLoader::AC_LevelLoader()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AC_LevelLoader::BeginPlay()
+{
+	Super::BeginPlay();
+	GenerateMaze();
+}
+
+// Called every frame
+void AC_LevelLoader::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AC_LevelLoader::GenerateMaze()
+{
+	
+	FTexture2DMipMap& mipMap = mazeTexture->GetPlatformData()->Mips[0];
+	FByteBulkData* rawImageData = &mipMap.BulkData;
+
+	FColor* allPixel = static_cast<FColor*>(rawImageData->Lock(LOCK_READ_ONLY));
+	int32 mazeWidth = mipMap.SizeX;
+	int32 mazeHeight = mipMap.SizeY;
+
+	for (int32 Y = 0; Y < mazeHeight; Y++)
+	{
+		for (int32 X = 0; X < mazeWidth; X++)
+		{
+			int32 currentIndex = Y * mazeWidth + X;
+			FColor currentPixel = allPixel[currentIndex];
+
+			FVector spawnLocation = GetActorLocation() + FVector(X * tileSize, Y * tileSize, 0);
+
+			// Example: Define wall, path, pellet using color
+			if (currentPixel == FColor::Black )
+			{
+				GetWorld()->SpawnActor<AActor>(wall, spawnLocation, FRotator::ZeroRotator);
+			}
+			else if(currentPixel == FColor::Yellow)
+			{
+				GetWorld()->SpawnActor<AActor>(dots, spawnLocation, FRotator::ZeroRotator);
+			}
+			
+		}
+	}
+
+	rawImageData->Unlock();
+
+
+
+}
+
---
+++ Source/PR_Assesment/C_LevelLoader.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "C_LevelLoader.generated.h"
+
+UCLASS()
+class PR_ASSESMENT_API AC_LevelLoader : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AC_LevelLoader();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void GenerateMaze();
+
+	UPROPERTY(EditAnywhere)
+	UTexture2D* mazeTexture;
+
+	UPROPERTY(EditAnywhere)
+	float tileSize = 100;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> wall;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> dots;
+};
